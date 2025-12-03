5c60515ab7ca55c76922b60ef6a80cb9d3b05d3d
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2025-12-03 19:00:24 +0100

    commit prueba

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+let mano = 0
+input.onGesture(Gesture.Shake, function () {
+    let piedra = 0
+    mano = randint(1, 3)
+    if (mano == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (piedra == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
