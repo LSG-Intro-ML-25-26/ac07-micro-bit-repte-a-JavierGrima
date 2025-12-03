07f695f5829eb8779687959349630b4d67b1ce38
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2025-12-03 19:22:17 +0100

    Piedra, papel y tijera contra bot

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-mano = 0
+jugador = 0
+bot = 0
 
-def on_gesture_shake():
-    global mano
-    mano = randint(1, 3)
-    if mano == 1:
-        basic.show_icon(IconNames.SMALL_SQUARE)
-    elif mano == 2:
+def on_button_pressed_a():
+    global jugador
+    jugador = randint(1, 3)
+    if jugador == 1:
         basic.show_icon(IconNames.SQUARE)
+    elif jugador == 2:
+        basic.show_icon(IconNames.SMALL_SQUARE)
     else:
         basic.show_icon(IconNames.SCISSORS)
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global bot
+    bot = randint(1, 3)
+    if bot == 1:
+        basic.show_icon(IconNames.SQUARE)
+    elif bot == 2:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
-let mano = 0
-input.onGesture(Gesture.Shake, function () {
-    let piedra = 0
-    mano = randint(1, 3)
-    if (mano == 1) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else if (piedra == 2) {
+let jugador = 0
+let bot = 0
+input.onButtonPressed(Button.A, function () {
+    jugador = randint(1, 3)
+    if (jugador == 1) {
         basic.showIcon(IconNames.Square)
+    } else if (jugador == 2) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (jugador == 1 && bot == 2 || (jugador == 2 && bot == 3 || jugador == 3 && bot == 1)) {
+        basic.showIcon(IconNames.Yes)
+    } else if (jugador == 1 && bot == 3 || (jugador == 3 && bot == 2 || jugador == 2 && bot == 1)) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    bot = randint(1, 3)
+    if (bot == 1) {
+        basic.showIcon(IconNames.Square)
+    } else if (bot == 2) {
+        basic.showIcon(IconNames.SmallSquare)
     } else {
         basic.showIcon(IconNames.Scissors)
     }
